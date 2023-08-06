feb981cd6fceac9cea142558da15277822556a23
coderismail22 <122710226+coderismail22@users.noreply.github.com>
2023-08-07 05:27:33 +0600

    firebase-added

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDt0_WnrxAFvFno4Tg7Hq6inPuLZgMrCcQ",
+  authDomain: "winner-photography.firebaseapp.com",
+  projectId: "winner-photography",
+  storageBucket: "winner-photography.appspot.com",
+  messagingSenderId: "944269262299",
+  appId: "1:944269262299:web:ec8b52fba4612c9d757bfe",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);

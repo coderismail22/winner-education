1ae4e6a4b99ad3f56a3796e308969908c1fe1f6e
coderismail22 <122710226+coderismail22@users.noreply.github.com>
2023-08-07 05:28:55 +0600

    useAdmin-added

---
+++ src/hooks/useAdmin.jsx
@@ -0,0 +1,21 @@
+import { useQuery } from "@tanstack/react-query";
+// import useAxiosSecure from "./useAxiosSecure";
+import { useContext } from "react";
+import { AuthContext } from "../Providers/AuthProvider";
+import axios from "axios";
+
+const useAdmin = () => {
+    const {user, loading} = useContext(AuthContext)
+    // const [axiosSecure] = useAxiosSecure();
+    // use axios secure with react query
+    const {data: isAdmin, isLoading: isAdminLoading} = useQuery({
+        queryKey: ['isAdmin', user?.email],
+        enabled: !loading,
+        queryFn: async () => {
+            const res = await axios.get(`https://assignment-12-server-chi-ten.vercel.app/users/admin?email=${user?.email}`);
+            return res.data.admin;
+        }
+    })
+    return [isAdmin, isAdminLoading]
+}
+export default useAdmin;
